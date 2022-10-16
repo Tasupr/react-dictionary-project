8f5e432568587e846466ec9d8b2fc7ef3e2440b7
OlegSuprunovich <olsupr@ukr.net>
2022-10-16 18:11:15 +0300

    update design final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import logo from "./logo.png";
-import "./App.css";
 import Dictionary from "./Dictionary";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            <img src={logo} className="logo" alt="SheCodes Logo" />
+          </a>
         </header>
-        <main>
-          <Dictionary />
-        </main>
-        <footer className="App-footer">
-          <small>Coded by Tanya Suprunovich</small>
+        <Dictionary defaultKeyword="sunset" />
+        <footer>
+          This project was coded by Tanya Suprunovich
+          and is{" "}
+          <a
+            href="https://github.com/Tasupr/react-dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           my GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://joyful-hamster-cdb9da.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
   );
 }
+
+export default App;
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,24 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <p>{definition.definition}</p>
               <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+              <Synonyms synonyms={definition.synonyms} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
